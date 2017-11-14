045dd570ad2a33c7cd8bc3b0ad4164e992a3c2bc
Kalaborative <kalaborative94@gmail.com>
2017-11-14 15:54:35 -0600

    Included purposely failing tests

--- test_selenium.py
+++ test_selenium.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+import requests
 from flask import get_flashed_messages
 
 driver = webdriver.Chrome()
-letters_in_ID = "Please submit a valid ID. No letters."
+non_number_in_ID = "Please submit a valid ID. Numbers only!"
 invalid_length = "Invalid length. Try again."
 
 def test_setup():
@@ -15,7 +16,7 @@ def test_flash_1():
 	driver.find_element_by_tag_name('input').send_keys('dlfkdjd')
 	driver.find_element_by_id('login-button').click()
 	message = driver.find_element_by_tag_name('p')
-	assert message.text == letters_in_ID
+	assert message.text == non_number_in_ID
 
 def test_flash_2():
 	driver.find_element_by_tag_name('input').send_keys('3092')
@@ -27,13 +28,13 @@ def test_flash_3():
 	driver.find_element_by_tag_name('input').send_keys('302233a')
 	driver.find_element_by_id('login-button').click()
 	message = driver.find_element_by_tag_name('p')
-	assert message.text == letters_in_ID
+	assert message.text == non_number_in_ID
 
 def test_flash_4():
 	driver.find_element_by_tag_name('input').send_keys('Mangohero1')
 	driver.find_element_by_id('login-button').click()
 	messages = driver.find_elements_by_tag_name('p')
-	assert messages[0].text == letters_in_ID
+	assert messages[0].text == non_number_in_ID
 	assert messages[1].text == invalid_length
 
 def test_account():
@@ -46,6 +47,17 @@ def test_result():
 	driver.get('localhost:5000/results')
 	title = driver.find_element_by_tag_name('h1').text
 	assert "by" not in title
+
+def test_code():
+	r = requests.get('localhost:5000/randomstuff')
+	assert r.status_code == 404
+
+def test_special_char():
+	driver.get('localhost:5000')
+	driver.find_element_by_tag_name('input').send_keys('000-000')
+	driver.find_element_by_id('login-button').click()
+	message = driver.find_element_by_tag_name('p')
+	assert message.text == non_number_in_ID
 
 def test_teardown():
 	called = []
